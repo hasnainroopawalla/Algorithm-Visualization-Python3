7707e35394c6037929742ee26cf9facf12a5f6e3
Hasnain Roopawalla <hasnain.roopawalla@gmail.com>
2020-04-01 15:57:06 +0530

    shell

---
+++ Sorting/shell sort/shellsort.py
@@ -0,0 +1,20 @@
+snum = []
+def shellSort(a, n):
+    gap = n // 2
+    while gap > 0:
+        for i in range(gap, n):
+            temp = a[i]
+            j = i
+            while j >= gap and a[j - gap] > temp:
+                a[j] = a[j - gap]
+                j -= gap
+
+            a[j] = temp
+            snum.append(list(a))
+        gap //= 2
+        
+
+def sortnum(a):
+    size = len(a)
+    shellSort(a, size)
+    return snum
